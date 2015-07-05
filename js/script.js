61c791507d70745ce03ee1811737fdc38450ae24
Alexander Busygin <pincher2012@yandex.ru>
2015-07-05 07:08:27 +0500

    restrict input, only numeric from diapason [-10, 10]

--- js/script.js
+++ js/script.js
@@ -195,4 +195,48 @@ function MatrixViewModel() {
 
 };
 
+ko.bindingHandlers.numeric = {
+    init: function (element, valueAccessor) {
+        $(element).on("keydown", function (event) {
+            var value = parseInt(ko.unwrap(valueAccessor()) + String.fromCharCode(event.keyCode));
+            // Allow: backspace, delete, tab, escape, and enter
+            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+                // Allow: Ctrl+A
+                (event.keyCode == 65 && event.ctrlKey === true) ||
+                // Allow: F1-F12
+                (event.keyCode >= 112 && event.keyCode <= 123) ||
+                // Allow: home, end, left, right
+                (event.keyCode >= 35 && event.keyCode <= 39)) {
+                // let it happen, don't do anything
+                return;
+            }
+            else if (ko.unwrap(valueAccessor()) == '' && (event.keyCode == 109 || event.keyCode == 189)) {
+                return
+            }
+            else {
+                // Ensure that it is a number and stop the keypress
+                if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+                    event.preventDefault();
+                }
+
+                if (value > 10) {
+                    element.value = 10;
+                    event.preventDefault();
+                }
+
+                if (value < -10) {
+                    element.value = -10;
+                    event.preventDefault();
+                }
+
+                if (value === 0) {
+                    element.value = value;
+                    event.preventDefault();
+                }
+            }
+        });
+    }
+};
+
+
 ko.applyBindings(new MatrixViewModel());
